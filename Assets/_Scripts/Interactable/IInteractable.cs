86e69de641fb50990bcdb3eb4ef0109e52963c70
Master Of Evil <guillaume.duquesne@outlook.fr>
2018-08-11 14:46:46 -0400

    squirel

---
+++ Assets/_Scripts/Interactable/IInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    Rigidbody Rigidbody { get; set;}
+    Transform Cursor { get; set; }
+
+    void BeginInteraction(Transform cursor);
+    void EndInteraction();
+    void UpdateInteraction();
+}
---
+++ Assets/_Scripts/Interactable/SimpleInteractable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleInteractable : MonoBehaviour, IInteractable 
+{
+    public Rigidbody Rigidbody { get; set; }
+    public Transform Cursor { get; set; }
+
+    public float speed = 1.0f;
+
+    void Start () 
+	{
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void BeginInteraction(Transform cursor)
+	{
+        Rigidbody.isKinematic = false;
+        Rigidbody.useGravity = false;
+        Cursor = cursor;
+    }
+
+    public void EndInteraction()
+    {
+        Rigidbody.useGravity = true;
+    }
+
+    public void UpdateInteraction()
+    {
+        Rigidbody.velocity = (Cursor.position - transform.position) * Time.fixedDeltaTime * speed;
+    }
+}
