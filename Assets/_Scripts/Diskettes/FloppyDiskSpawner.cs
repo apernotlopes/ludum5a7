ab5180f88489efa980e09cb7c1e0a66942ab30b6
Alexandre <alexandre@trebuchet.fun>
2018-08-11 20:12:04 -0400

    dldlddldldlijang

--- Assets/_Scripts/Diskettes/FloppyDiskSpawner.cs
+++ Assets/_Scripts/Diskettes/FloppyDiskSpawner.cs
@@ -21,7 +21,7 @@ public class FloppyDiskSpawner : MonoBehaviour
         List<Floppy> floppys = new List<Floppy>();
 
         for (int i = 0; i < totalFile; i++)
-            files.Add(new FileData("nike alex"));
+            files.Add(new FileData());
 
 
         for (int i = 0; i < numberToSpawn; i++)
