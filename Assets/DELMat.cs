65abe82550fc57a01379479f67752cb947073926
Alexandre <alexandre@trebuchet.fun>
2018-08-13 12:57:30 -0400

    TXXXT

--- Assets/DELMat.cs
+++ Assets/DELMat.cs
@@ -17,11 +17,11 @@ public class DELMat : MonoBehaviour
 
 	public void Blink(int loops = 3)
 	{
-		rend.materials[3].DOFloat(1f, "_SelfIllumPower", 0.2f).SetLoops(loops, LoopType.Yoyo).OnComplete(Off);
+		rend.materials[3].DOFloat(1.5f, "_SelfIllumPower", 0.2f).SetLoops(loops, LoopType.Yoyo).OnComplete(Off);
 	}
 
 	private void Off()
 	{
-		rend.materials[3].DOFloat(0.2f, "_SelfIllumPower", 0.2f);
+		rend.materials[3].DOFloat(0.5f, "_SelfIllumPower", 0.2f);
 	}
 }
